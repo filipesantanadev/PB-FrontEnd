18f6985de9b68107fa0d1489609f042a2209c67b
Ian Santos <ian.santos@qwst.co>
2022-08-17 20:42:53 -0300

    install naive Ui

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>about page</div>
+    <div>
+        <div style="width: 300px">
+            <n-carousel autoplay>
+                <img
+                class="carousel-img"
+                src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+                >
+                <img
+                class="carousel-img"
+                src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+                >
+                <img
+                class="carousel-img"
+                src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+                >
+                <img
+                class="carousel-img"
+                src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+                >
+                <img
+                class="carousel-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnCoYpX4iNaKkeOBaXaHZWQqjiGfhmWzVdpg&usqp=CAU"
+                >
+            </n-carousel>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    name: 'PBFrontEndIndex',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
 </script>
 
-<style></style>
+<style scoped>
+    .carousel-img {
+        width: 100%;
+        height: 240px;
+        object-fit: cover;
+    }
+</style>
