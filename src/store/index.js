30d3fe50f5c85f06f05ffb1744d28c07fae1f098
Ian Santos <ian.santos@qwst.co>
2022-09-15 18:11:34 -0300

    s

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
@@ -6,8 +7,17 @@ export default createStore({
   getters: {
   },
   mutations: {
+    teste(state, data){
+      console.log(data)
+    }
   },
   actions: {
+    async getBestRoute({commit}){
+      const origin = 'rua+jamil+nasser'
+      const destination = '-22.978323,-43.395791'
+      const { data } = await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}?destination=${destination}`)
+      commit('teste', data)
+    }
   },
   modules: {
   }
