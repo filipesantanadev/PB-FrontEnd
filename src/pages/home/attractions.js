6941a3b42d7653f0842d66e470322810708d95b3
Ian Santos <ian.santos@qwst.co>
2022-09-15 19:56:06 -0300

    attractions

---
+++ src/pages/home/attractions.js
@@ -0,0 +1,43 @@
+export const days = 
+    [
+        {
+            key: '02-09',
+            imgs: [
+                'https://pbs.twimg.com/media/Fa3WK-6XoAEn3Qj?format=jpg&name=900x900',
+                'https://pbs.twimg.com/media/Fa3WK-6XoAEn3Qj?format=jpg&name=900x900',
+                'https://pbs.twimg.com/media/Fa3WK-6XoAEn3Qj?format=jpg&name=900x900',
+                'https://pbs.twimg.com/media/Fa3WK-6XoAEn3Qj?format=jpg&name=900x900',
+            ],
+            stages: 
+                [
+                    { 
+                        name: 'Mundo',
+                        att: ['Iron Mainden', 'Dream Theater', 'Gogira']
+                    },
+                    {
+                        name: 'Sunset',
+                        att: ['Bullet For my Valentine', 'Living Colour feat Steve Val', 'METAL ALLEGIANCE', 'BLACK PANTERA CONVIDA DEVOTOS']
+                    }
+                ],
+        },
+        {
+            key: '03-09',
+            imgs: [
+                'https://portalrapmais.com/wp-content/uploads/2022/06/post-malone-1.webp',
+                'https://portalrapmais.com/wp-content/uploads/2022/06/post-malone-1.webp',
+                'https://portalrapmais.com/wp-content/uploads/2022/06/post-malone-1.webp',
+                'https://portalrapmais.com/wp-content/uploads/2022/06/post-malone-1.webp'
+            ],
+            stages: 
+                [
+                    {
+                        name: 'Mundo',
+                        att: ['POST MALONE', 'MARSHMELLO', 'JASON DERULO', 'ALOK']
+                    },
+                    {
+                        name: 'Sunset',
+                        att: ['RACIONAIS', 'CRIOLO CONVIDA MAYRA ANDRADE', 'XAMÃ CONVIDA BRÔ MCS', 'PAPATINHO + L7NNON CONVIDAM MC HARIEL E MC CAROL']
+                    }
+                ]
+        }
+    ]
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="main-container">
-    <div class="item-container" v-for="i in 9" :key="i">
-      <span>Titulo filme</span>
-      <img src="../../assets/logo.png" width="100" />
-      imagem filme First Commit Teste
+    <button v-for="(day, i) in days" :key="day.key" @click="selected = i">{{day.key}}</button>
+
+    <div style="width: 300px">
+      <n-carousel autoplay>
+        <img
+          v-for="img in days[selected].imgs"
+          :key="img"
+          class="carousel-img"
+          :src="img"
+        >
+      </n-carousel>
+    </div>
+
+    <div v-for="stage in days[selected].stages" :key="stage.name">
+      {{stage.name}}
+      <div v-for="att in stage.att" :key="att">
+        {{att}}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { days } from './attractions.js'
+
+export default {
+  data() {
+    return {
+      selected: 0,
+      days: days
+    };
+  },
+};
 </script>
 
 <style scoped>
-.main-container {
-  width: 100%;
-  grid-template-columns: repeat(3, 1fr);
-  display: grid;
-}
-
-.item-container {
-  display: flex;
-  background: #42b983;
-  margin: 1%;
-  border: 0.2rem solid transparent;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.item-container:hover {
-  border: 0.2rem solid #2c3e50;
-}
+  .carousel-img {
+    width: 100%;
+    height: 240px;
+    object-fit: cover;
+  }
 </style>
