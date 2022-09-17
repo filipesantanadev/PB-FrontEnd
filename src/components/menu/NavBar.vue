d3a77267f319063891c852820cb37cbea669647e
Ian Santos <ian.santos@qwst.co>
2022-09-17 15:34:21 -0300

    add map page

--- src/components/menu/NavBar.vue
+++ src/components/menu/NavBar.vue
@@ -1,43 +1,38 @@
 <template>
   <nav class="menu">
     <div class="logo">
-      <img src="../../assets/Rock.jpg" />
+      <img src="../../assets/Rock.jpg">
     </div>
 
-    <ItemMenu
-      class="menu-item"
-      v-for="item in routes"
-      :item="item"
-      :key="item.name"
-    />
+    <ItemMenu class="menu-item" v-for="item in routes" :item="item" :key="item.name"/>
   </nav>
 </template>
 
 <script>
-import ItemMenu from "./itemMenu";
+import ItemMenu from './itemMenu'
 
 export default {
-  components: { ItemMenu },
+  components: {ItemMenu},
 
   data() {
     return {
       width: window.innerWidth,
       routes: [
         {
-          name: "home",
-          icon: "fa-solid fa-music",
-          title: "Atrações",
+          name: 'home',
+          icon: 'fa-solid fa-music',
+          title: 'Atrações'
         },
         {
-          name: "info",
-          icon: "fa-solid fa-circle-info",
-          title: "Informações",
+          name: 'info',
+          icon: 'fa-solid fa-circle-info',
+          title: 'Informações'
         },
         {
-          name: "services",
-          icon: "fa-solid fa-hand-holding-medical",
-          title: "Serviços",
-        },
+          name: 'services',
+          icon: 'fa-solid fa-hand-holding-medical',
+          title: 'Serviços'
+        }
       ],
     };
   },
@@ -51,31 +46,26 @@ export default {
 </script>
 
 <style scoped>
-.logo {
-  display: flex;
-  border-bottom: 2px solid #e30613;
-}
+  .logo{
+    display: flex;
+  }
 
-.logo img {
-  margin-top: 2px;
-  width: 40%;
-  margin: 0 auto;
-  min-width: 60px;
-}
+  .logo img{
+    margin-top: 2px;
+    width: 40%;
+    margin: 0 auto;
+    min-width: 60px;
+  }
 
-.menu {
-  background-color: #000000;
-}
+  .menu-item{
+    display: flex;
+    align-items: center;
+    margin-top: 16px;
+    padding-bottom: 6px;
+    border-bottom: 2px solid #000000;
+  }
 
-.menu-item {
-  display: flex;
-  align-items: center;
-  margin-top: 16px;
-  padding-bottom: 6px;
-  border-bottom: 2px solid #000000;
-}
-
-.menu-item:hover {
-  border-bottom: 2px solid #e30613;
-}
-</style>
+  .menu-item:hover {
+    border-bottom: 2px solid #79D4C8;
+  }
+  </style>
---
+++ src/pages/LocationsServices/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="services-container">
+        <h1> Serviços proximos </h1>
+        <div id="map"></div>
+
+        <div>
+            <h1 style="color: rgb(121, 212, 200)">ROCK IN RIO</h1>
+            <h1 style="color: red">HOSPITAIS</h1>
+            <h1 style="color: yellow">delegacia</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import { addToMap } from './locations'
+
+export default {
+    name: 'PBFrontEndIndex',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        var map = L.map('map').setView([-22.977, -43.395], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
+        
+        addToMap(map)
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+    #map{
+        margin: 25px auto;
+        width: 98%;
+        max-width: 920px;
+        height: 500px;
+        border-radius: 15px;
+    }
+
+    h1{
+        font-weight: bold;
+        margin-bottom: 12px;
+        font-family: 'Kanit', sans-serif;
+        border-bottom: 1px solid #C3C1C5;
+        padding: 10px;
+        color: #C3C1C5
+    }
+
+    .services-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center
+    }
+</style>
